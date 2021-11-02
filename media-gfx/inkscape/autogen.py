45005a2c00d815d0524e481cde8eeaa5ff06feb5
shadow <shadow.ngo@gmail.com>
2021-11-02 11:20:16 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: a34a4d29dc42f08e738b4d257c56f82b600d6d48
      deadbeef: 53e0a41db6e7193bd83ef117f8055457f0990cb7
      gentoo-staging-late-2020: 2e49e96c41bf524a74ee347c49438612d7d8d392
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 96920cf2a819e388bb05d6763d90872d2882ff0d

---
+++ media-gfx/inkscape/autogen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import json
+
+async def generate(hub, **pkginfo):
+	json_data = await hub.pkgtools.fetch.get_page("https://gitlab.com/api/v4/projects/3472737/releases", is_json=True)
+	version = None
+	commit = None
+	date = None
+
+	for item in json_data:
+		try:
+			commit = item["commit"]["id"][:10]
+			date = item["commit"]["created_at"].split("T")[0]
+			version = ".".join(item["tag_name"].split("_")[1:])
+			list(map(int, version.split(".")))
+			break
+
+		except (KeyError, IndexError, ValueError):
+			continue
+	else:
+		version = None
+
+	if version:
+		final_name = f"inkscape-{version}.tar.xz"
+		url = f"https://media.inkscape.org/dl/resources/file/{final_name}"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			srcdir=f"inkscape-{version}_{date}_{commit}",
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		)
+		ebuild.push()
+
+# vim: ts=4 sw=4 noet
