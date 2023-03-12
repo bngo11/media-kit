e6d5a5b83c15ee64253e258fd9f27a0a6fba34f3
shadow <shadowngoz@gmail.com>
2023-03-11 21:01:50 -0600

    Autogenerated tree updates.
    merged:
    
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      kit-fixups: 499ef30a50b36e040959171f34923d6317c88cbe

--- media-gfx/inkscape/autogen.py
+++ media-gfx/inkscape/autogen.py
@@ -1,35 +1,28 @@
 #!/usr/bin/env python3
 
-import json
+from bs4 import BeautifulSoup
+from packaging.version import Version
+import os
+import re
+
+regex = r'(\d+(?:\.\d+)+)'
+
 
 async def generate(hub, **pkginfo):
-	json_data = await hub.pkgtools.fetch.get_page("https://gitlab.com/api/v4/projects/3472737/releases", is_json=True)
-	version = None
-	commit = None
-	date = None
+    base_url = "https://inkscape.org/"
+    download_url = base_url + "release/all/source/archive/"
+    html = await hub.pkgtools.fetch.get_page(download_url)
+    soup = BeautifulSoup(html, features='html.parser').find_all('a', href=True)
 
-	for item in json_data:
-		try:
-			commit = item["commit"]["id"][:10]
-			date = item["commit"]["created_at"].split("T")[0]
-			version = ".".join(item["tag_name"].split("_")[1:])
-			list(map(int, version.split(".")))
-			break
+    tarballs = [a.get('href') for a in soup if '.tar.' in a.contents[0] and not a.contents[0].endswith('sig')]
+    versions = [(Version(re.findall(regex, a)[0]), a) for a in tarballs if re.findall(regex, a)]
+    latest = max(versions)
 
-		except (KeyError, IndexError, ValueError):
-			continue
-	else:
-		version = None
+    artifact = hub.pkgtools.ebuild.Artifact(url=base_url+latest[1])
 
-	if version:
-		final_name = f"inkscape-{version}.tar.xz"
-		url = f"https://media.inkscape.org/dl/resources/file/{final_name}"
-		ebuild = hub.pkgtools.ebuild.BreezyBuild(
-			**pkginfo,
-			version=version,
-			srcdir=f"inkscape-{version}_{date}_{commit}",
-			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
-		)
-		ebuild.push()
-
-# vim: ts=4 sw=4 noet
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        version=latest[0],
+        artifacts=[artifact],
+    )
+    ebuild.push()
